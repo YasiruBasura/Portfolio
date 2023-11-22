f8de89935e2957f95df26f372fe8bc8b83f5ea97
Yasi <113851370+YasiruBasura@users.noreply.github.com>
2023-11-23 03:51:49 +0530

    responsive-nav-bar

--- js/main.js
+++ js/main.js
@@ -77,4 +77,26 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
         });
     });
 });
+
+
+//Responsive navigation bar
+const menuBtn = document.querySelector('.nav-menu-btn');
+const closeBtn = document.querySelector('.nav-close-btn');
+const navigation = document.querySelector('.navigation');
+const navItems = document.querySelectorAll('.nav-items a');
+
+menuBtn.addEventListener('click', () => {
+    navigation.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => { 
+    navigation.classList.remove('active');
+}); 
+
+navItems.forEach((navItem) => { 
+    navItem.addEventListener('click', () => { 
+        navigation.classList.remove('active');
+    });
+}); 
+
     
